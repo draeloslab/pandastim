d215517d3a4b79199b9fc33427afc56edbd157fe
mattdloring <72998028+mattdloring@users.noreply.github.com>
2021-03-04 10:36:52 -0500

    Update publish_random2.py

--- examples/publish_random2.py
+++ examples/publish_random2.py
@@ -12,17 +12,19 @@ Part of pandastim package: https://github.com/EricThomson/pandastim
 
 import time
 import random
-from utils import Publisher as Pub
 
-pub = Pub(port = "1234")
-print("Starting publisher loop to generate random outputs...")
-while True:
-    delay_time = random.uniform(0.4, 2)  #.5 3
-    output = random.randint(0, 1)
-    topic = b"stim"
-    msg = str(output).encode('ascii')
-    data = topic + b" " + msg
-    pub.socket.send(data)
-    print(f"pub.py: sent data: {data}")
-    time.sleep(delay_time)
-pub.kill()
+from pandastim.utils import Publisher as Pub
+
+
+def randomPublish2(port="1234"):
+    pub = Pub(port=port)
+    print("Starting publisher loop to generate random outputs...")
+    while True:
+        delay_time = random.uniform(0.4, 2)  #.5 3
+        output = random.randint(0, 1)
+        topic = b"stim"
+        msg = str(output).encode('ascii')
+        data = topic + b" " + msg
+        pub.socket.send(data)
+        print(f"pub.py: sent data: {data}")
+        time.sleep(delay_time)

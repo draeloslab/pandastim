01af9f1111974f8add2e8b4a86a6e6eb46dd9613
mattdloring <72998028+mattdloring@users.noreply.github.com>
2021-03-04 10:36:48 -0500

    Update moving_red_sin.py

--- examples/moving_red_sin.py
+++ examples/moving_red_sin.py
@@ -6,16 +6,15 @@ For info about profiling and the profile_on keyword, see pandastim/readme.md
 
 Part of pandastim package: https://github.com/EricThomson/pandastim
 """
-import textures
-import stimuli
+from pandastim import textures, stimuli
 
 sin_red_tex = textures.SinRgbTex(texture_size = 512,
                                  spatial_frequency = 30,
                                  rgb = (255, 0, 0))
 sin_red_stim = stimuli.TexMoving(sin_red_tex,
-                                     angle = 33, 
+                                     angle = 33,
                                      velocity = -0.05,
                                      fps = 30,
                                      window_name = 'red drifting sin example',
-                                     profile_on = True)
+                                     profile_on = False)
 sin_red_stim.run()

42a9c7569f01c7a6d97ac73c5df8c75a36ec2614
mattdloring <72998028+mattdloring@users.noreply.github.com>
2021-03-12 18:28:10 -0500

    Update closedloopchoice.py

--- experiments/closedloopchoice.py
+++ experiments/closedloopchoice.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from pandastim import textures, stimuli, utils
 
+svepath = r'C:\Soft_Kitty\Anaconda3\envs\clean_pstim\Lib\site-packages\pandastim\experiments\temp1.txt'
 
 tex_size = (1024, 1024)
 freqs = np.arange(101)
@@ -11,7 +12,7 @@ for f in freqs:
     input_textures['freq'][f] = textures.GratingGrayTexXY(texture_size=tex_size, spatial_frequency=f)
 
 
-stimulation = stimuli.ClosedLoopStimChoice(textures=input_textures)
+stimulation = stimuli.ClosedLoopStimChoice(textures=input_textures, save_path=svepath)
 
 if __name__ == '__main__':
 

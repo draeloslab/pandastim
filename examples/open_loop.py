362e986247a3a8f1ce5cf89356e7b5555766cd91
Anjali Shankar <anjshnkr@umich.edu>
2024-12-04 18:00:33 -0500

    added examples for different ellipses and rectangular stimuli

--- examples/open_loop.py
+++ examples/open_loop.py
@@ -54,31 +54,43 @@ sin_red_stimulus = stimulus_details.MonocularStimulusDetails(
 #     fg_intensity=50,
 # )
 
-circle_tex= textures.CircleGrayTex(
-    texture_size = 5000, 
-    frequency = 1,
-    circle_center= (2750,3000),
-    circle_radius= 10,
-    texture_name='gray_circle',
-    bg_intensity= 200,
-    fg_intensity=50,
-)
-circle_stimulus = stimulus_details.MonocularStimulusDetails(
-    texture=circle_tex,
-    angle=90,
-    velocity=0.08,
-    stationary_time=0,
-    duration=20,
-    hold_after=float(20),
-    stim_name="gray_circ",
-)
+# circle_tex= textures.CircleGrayTex(
+#     texture_size = 5000, 
+#     frequency = 200,
+#     circle_center= (2600,3000),
+#     circle_radius= 20,
+#     spacing= 100,
+#     texture_name='gray_circle',
+#     bg_intensity= 200,
+#     fg_intensity=50,
+# )
+# circle_stimulus = stimulus_details.MonocularStimulusDetails(
+#     texture=circle_tex,
+#     angle=90,
+#     velocity=0.05,
+#     stationary_time=0,
+#     duration=5,
+#     hold_after=float(5),
+#     stim_name="gray_circ",
+# )
+
+# ellipse_tex= textures.EllipseGrayTex(
+#     texture_size = 5000, 
+#     frequency = 1,
+#     center= (2750,3000),
+#     h_radius= 40,
+#     v_radius= 20,
+#     texture_name='gray_ellipse',
+#     bg_intensity= 200,
+#     fg_intensity=50,
+# )
 
 ellipse_tex= textures.EllipseGrayTex(
-    texture_size = 5000, 
-    frequency = 1,
-    center= (2700,3000),
-    h_radius= 25,
-    v_radius= 150,
+    texture_size = 1600, 
+    frequency = 100,
+    center= (865,955),
+    width= 50,
+    length= 800,
     texture_name='gray_ellipse',
     bg_intensity= 200,
     fg_intensity=50,
@@ -86,32 +98,32 @@ ellipse_tex= textures.EllipseGrayTex(
 ellipse_stimulus = stimulus_details.MonocularStimulusDetails(
     texture=ellipse_tex,
     angle=90,
-    velocity=0.08,
+    velocity=0.05,
     stationary_time=0,
-    duration=20,
-    hold_after=float(20),
-    stim_name="gray_circ",
+    duration=5,
+    hold_after=float(5),
+    stim_name="gray_ellipse",
 )
 
-# rect_tex= textures.RectGrayTex(
-#     texture_size = 5000, 
-#     frequency = 1000,
-#     center= (2700,3000),
-#     length= 1024,
-#     width= 100,
-#     texture_name='gray_rectangle',
-#     bg_intensity= 200,
-#     fg_intensity=50,
-# )
-# rect_stimulus = stimulus_details.MonocularStimulusDetails(
-#     texture=rect_tex,
-#     angle=90,
-#     velocity=0.1,
-#     stationary_time=0,
-#     duration=20,
-#     hold_after=float(20),
-#     stim_name="gray_circ",
-# )
+rect_tex= textures.RectGrayTex(
+    texture_size = 1600, 
+    frequency = 200,
+    center= (865,955),
+    length= 85,
+    width= 2000,
+    texture_name='gray_rectangle',
+    bg_intensity= 200,
+    fg_intensity=50,
+)
+rect_stimulus = stimulus_details.MonocularStimulusDetails(
+    texture=rect_tex,
+    angle=90,
+    velocity=0.08,
+    stationary_time=0,
+    duration=5,
+    hold_after=float(5),
+    stim_name="gray_rectangle",
+)
 
 
 # We can create binocular by combining monocular
@@ -142,24 +154,24 @@ fresh_binoc_stim = stimulus_details.BinocularStimulusDetails(
     texture=(sin_gray_tex, grate_gray_tex),
 )
 
-callibration_text = textures.CallibrationDots(
-    texture_size = 5000, 
-    circle_center= (2750,3000),
-    circle_radius= 20,
-    bg_intensity=200,
-    texture_name='callibration_dots',
-)
-callibration_stim = stimulus_details.MonocularStimulusDetails(
-    texture=callibration_text,
-    angle=90,
-    velocity=0.0,
-    stationary_time=0,
-    duration=20,
-    hold_after=float(20),
-    stim_name="callibration_dots",
-)
-
-all_stimuli = [callibration_stim]
+# callibration_text = textures.CallibrationDots(
+#     texture_size = 1600,
+#     circle_center= (865,955),
+#     circle_radius= 25,
+#     bg_intensity=200,
+#     texture_name='callibration_dots',
+# )
+# callibration_stim = stimulus_details.MonocularStimulusDetails(
+#     texture=callibration_text,
+#     angle=90,
+#     velocity=0.0,
+#     stationary_time=0,
+#     duration=60,                                    
+#     hold_after=float(60),
+#     stim_name="callibration_dots",
+# )
+           
+all_stimuli = [rect_stimulus]
 #[circle_stimulus, ellipse_stimulus]
 
 

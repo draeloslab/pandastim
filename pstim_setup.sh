2bfe6bfb1c2d4627b9e8efd8f5f3af089268097b
Anjali <anjshnkr@umich.edu>
2025-07-11 11:06:35 -0400

    changed env namein setup script

--- pstim_setup.sh
+++ pstim_setup.sh
@@ -3,6 +3,6 @@
 cd $HOME/Desktop/Code/pandastim/ || exit
 
 source ~/miniforge3/etc/profile.d/conda.sh  
-conda activate pstim
+conda activate improv
 
 export PYTHONPATH=$HOME/Desktop/Code/:$PYTHONPATH
